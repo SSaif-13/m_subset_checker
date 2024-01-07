d6b9f9d891919339afad1b2085aaa30bf7b34583
Sadat Saif <sadat.saif@ucalgary.ca>
2024-01-06 18:20:11 -0700

    Check if m_Subset is a multiset subset of m_Superset

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Import the Constructable function from the checker module.
 from checker import Constructable
 
-# Define the superset and subset as strings.
-# In this case, both the superset and subset are 'I was one'.
+# Define the superset and subset as strings
+# In this case, both the superset and subset are 'I was one'
 m_Superset = 'I was one'
 m_Subset = 'I was one'
 
-# Call the Constructable function with the subset and superset.
-# The Constructable function checks if the subset is a multiset subset of the superset.
+# Call the Constructable function with the subset and superset
+# The Constructable function checks if the subset is a multiset subset of the superset
 # It returns "Yes, it is a multi subset" if the subset is a multiset subset of the superset,
 # and "Not a multi subset" otherwise.
 # The result is printed to the console.
